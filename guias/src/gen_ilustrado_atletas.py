5121d5d407b3b17e9e67f52236fc2c4b3160fde4
Bruno Guilherme <brunoleoguilherme@gmail.com>
2026-07-17 19:57:13 -0300

    guias de atleta em EN e ES (texto + ilustrado) na pagina de Documentos

--- guias/src/gen_ilustrado_atletas.py
+++ guias/src/gen_ilustrado_atletas.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-"""Gera o guia ILUSTRADO dos ATLETAS (PT) reaproveitando o renderizador dos times."""
+"""Gera os guias ILUSTRADOS dos ATLETAS (PT/EN/ES) reaproveitando o renderizador dos times."""
 import gen_ilustrado, content_ilu_atletas
 gen_ilustrado.ILU = content_ilu_atletas.ILU
 if __name__ == '__main__':
-    gen_ilustrado.build('pt')
+    for lang in ('pt', 'en', 'es'):
+        gen_ilustrado.build(lang)
--- guias/src/gen_texto_atletas.py
+++ guias/src/gen_texto_atletas.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
-"""Gera o guia de TEXTO dos ATLETAS (PT) reaproveitando o renderizador dos times."""
-import gen_texto
-import content_txt_atletas
-
-# Reusa exatamente o mesmo layout/estilo do guia dos times, trocando só o conteúdo.
+"""Gera os guias de TEXTO dos ATLETAS (PT/EN/ES) reaproveitando o renderizador dos times."""
+import gen_texto, content_txt_atletas
 gen_texto.TXT = content_txt_atletas.TXT
-
 if __name__ == '__main__':
-    gen_texto.build('pt')
+    for lang in ('pt', 'en', 'es'):
+        gen_texto.build(lang)
